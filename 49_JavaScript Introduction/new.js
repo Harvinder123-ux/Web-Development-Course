dace0483e194d9206f35b07b7b1b99ccf29b0a96
Harvinder123-ux <leoabhiarya@gmail.com>
2024-07-03 15:56:02 +0530

    JavaScript Introduction

---
+++ 49_JavaScript Introduction/new.js
@@ -0,0 +1,6 @@
+// const newModule = require('./new.js');
+
+console.log("Hello World");
+console.log("Hello Its me harvinder");
+console.log("5 + 5");
+console.log("5" + 5);
---
+++ 49_JavaScript Introduction/script.js
@@ -0,0 +1,14 @@
+alert("hello world");
+
+console.log("hello world");
+console.log("Its me abhi");
+console.log("the code in console executing");
+
+let num = prompt("enter your number to multiply with 5");
+console.log("Your Number Multipication is :", 5 * num);
+
+
+let isTrue = confirm("do you want to leave this website and of your computer");
+if (isTrue) {
+    console.log("your pc computer is turned off");
+}
