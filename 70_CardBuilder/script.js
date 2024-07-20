9cd7f4d759e879eef59cbca3b38c67925e46e8fa
Harvinder123-ux <leoabhiarya@gmail.com>
2024-07-20 21:07:19 +0530

    Card Builder Exercise

--- 70_CardBuilder/script.js
+++ 70_CardBuilder/script.js
@@ -1,9 +1,26 @@
 function createCard(title, cName, views, monthsOld, duration) {
   let cardTitle = title;
   let channelName = cName;
-  let views1 = views;
   let old = monthsOld;
   let timing = duration;
+
+  let viewsStr;
+  if (views < 1000) {
+    viewsStr = views.toString();
+  } else if (views < 1000000) {
+    viewsStr = views / 1000 + "k";
+  } else {
+    viewsStr = views / 1000000 + "m";
+  }
+
+  // let viewsStr;
+  // if (views < 1000) {
+  //   viewsStr = views.toString();
+  // } else if (views < 1000000) {
+  //   viewsStr = (views / 1000).toFixed(1) + "k";
+  // } else {
+  //   viewsStr = (views / 1000000).toFixed(1) + "m";
+  // }
 
   let container = document.querySelector(".container");
   let card = document.createElement(`div`);
@@ -11,7 +28,7 @@ function createCard(title, cName, views, monthsOld, duration) {
     <img  src = "${"image 2.jpg"}" alt="Video Thumbnail" class="thumbnail">
     <div class="content">
       <h3 class="cardTitle">${cardTitle}</h3>
-      <p class="info">${channelName} • ${views1} views • ${old}</p>
+      <p class="info">${channelName} • ${viewsStr} views • ${old}</p>
       <span class="duration">${timing}</span>
     </div>
   </div>`;
@@ -23,21 +40,21 @@ function createCard(title, cName, views, monthsOld, duration) {
 createCard(
   "Hello Guys How You Doin? its me abhi | My first video ",
   "abhi vibes",
-  1 + "m",
+  1,
   12 + " months ago",
   "3 : 40 sec"
 );
 createCard(
   "Lets Talk about the cars ! Which one is your favourite car?",
   "abhi vibes",
-  975 + "k",
+  975,
   12 + "months ago",
   "4 : 59 sec"
 );
 createCard(
   "Do you guys know about the lambo hurcane the wonder car?",
   "abhi vibes",
-  565 + "k",
+  565,
   12 + "months ago",
   "5 : 53 sec"
 );
