22fead158e4a1f04533a0a826b406eed48393ea6
Harvinder123-ux <leoabhiarya@gmail.com>
2024-07-25 07:29:00 +0530

    Concat Practice

---
+++ 81_Concat JavaScript Practices/script.js
@@ -0,0 +1,39 @@
+let arr1 = ["Hello", "Hinata", 7];
+let arr2 = ["Its me", "Naruto", 1];
+
+let resArr = arr1.concat(arr2);
+console.log(resArr);
+
+//if we use the opearator instead of concat then its will return the value instead of units
+//text shadow contain four properties - right bottom opacity color
+//box shadow contain five properties - right bottom opacity color spread
+
+//learn the difference between the functions and methods
+
+//include method in array reaturn the boolean values
+
+
+let str = "The Man Who Can Burn Fire Itself";
+// let a  = str.search("Gh");
+// let a  = str.indexOf("Gh");
+// let s = str.replace("this", "here")
+// let s = str.trim(); //learn about this
+
+//slice - used for get the specific piece of string
+// let s = str.slice(0 , 3)
+
+//split - this method is use for split the string the string and return the value in array
+let s = str.split(" ");
+let a = s.join()// used for join the string
+console.log(a)
+
+
+//substr
+
+//Number Method
+
+//parseInt
+//parseFloat
+//ToFixed - round figure
+
+
