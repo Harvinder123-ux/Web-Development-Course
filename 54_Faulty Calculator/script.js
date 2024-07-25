3533eeecb1fc5be8bf626c9e39bf13daafbe6e72
Harvinder123-ux <leoabhiarya@gmail.com>
2024-07-25 07:29:53 +0530

    Faulty Calculator Practice

--- 54_Faulty Calculator/script.js
+++ 54_Faulty Calculator/script.js
@@ -38,51 +38,80 @@
 // }
 
 //without using eval
-let a = prompt("enter your first number");
-let b = prompt("enter your second number");
-let opr = prompt("enter the operator");
+// let a = prompt("enter your first number");
+// let b = prompt("enter your second number");
+// let opr = prompt("enter the operator");
 
-let obj = {
-  "+": "-",
-  "-": "+",
-  "**": "/",
-  "/": "*",
-  "*": "**",
+// let obj = {
+//   "+": "-",
+//   "-": "+",
+//   "**": "/",
+//   "/": "*",
+//   "*": "**",
+// };
+
+// a = parseInt(a);
+// b = parseInt(b);
+// let random = Math.random();
+// console.log(random);
+
+// if (isNaN(num1) || isNaN(num2)) {
+//   console.log("Please enter valid numbers");
+//   return;
+// }
+
+// if (random < 0.1) {
+//   let result = performOperation(a, b, opr);
+//   console.log(`the result is ${result}`);
+// } else {
+//   opr = obj[opr];
+//   //   console.log(`${a} ${opr} ${b}`);
+//   let result = performOperation(a, b, opr);
+//   console.log(`the result is ${result}`);
+// }
+
+// function performOperation(a, b, operator) {
+//   switch (operator) {
+//     case "+":
+//       return a + b;
+//     case "-":
+//       return a - b;
+//     case "*":
+//       return a * b;
+//     case "/":
+//       return a / b;
+//     case "**":
+//       return a ** b;
+//     default:
+//       return "Invalid operator";
+//   }
+// }
+
+const sum = (a, b) => {
+  console.log(a + b);
 };
 
-a = parseInt(a);
-b = parseInt(b);
-let random = Math.random();
-console.log(random);
+// setInterval(() => {
+//     sum();
+// }, 3000);
 
-if (isNaN(num1) || isNaN(num2)) {
-  console.log("Please enter valid numbers");
-  return;
-}
+setTimeout(() => {
+  sum(5, 7);
+}, 3000);
 
-if (random < 0.1) {
-  let result = performOperation(a, b, opr);
-  console.log(`the result is ${result}`);
-} else {
-  opr = obj[opr];
-  //   console.log(`${a} ${opr} ${b}`);
-  let result = performOperation(a, b, opr);
-  console.log(`the result is ${result}`);
-}
+//function that has no name called the anonymous function
+//arrow function intrduduced in es6 version of javaScript
 
-function performOperation(a, b, operator) {
-  switch (operator) {
-    case "+":
-      return a + b;
-    case "-":
-      return a - b;
-    case "*":
-      return a * b;
-    case "/":
-      return a / b;
-    case "**":
-      return a ** b;
-    default:
-      return "Invalid operator";
-  }
-}
+//aray in the javaScript
+let array = ["mango", "27", "90", "cherry"];
+console.log(...array[0]);
+
+let arr1 = [1, 2, 3, 4, 5];
+arr1.forEach((index, val) => {
+  index *= 2;
+  console.log(index);
+});
+
+
+let doubleNumbers = arr1.map(key => key * 2 );
+console.log(doubleNumbers);
