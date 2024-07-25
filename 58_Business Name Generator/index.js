c54e170186f3c3e8153a630fabfe511f97daa1d3
Harvinder123-ux <leoabhiarya@gmail.com>
2024-07-25 07:27:00 +0530

    Brand Name Generator

--- 58_Business Name Generator/index.js
+++ 58_Business Name Generator/index.js
@@ -39,33 +39,47 @@ let generateName = {
 
 console.log(generateName.generateN());
 
-
 //0 0.33 0.66 1
 let rand = Math.random();
 let first, second, third;
 
 if (rand < 0.33) {
-    first = "crazy";
-} else if ( rand > 0.33 && rand < 0.66 ) {
-    first = "amazing";
-}else {
-    first = "fire";
+  first = "crazy";
+} else if (rand > 0.33 && rand < 0.66) {
+  first = "amazing";
+} else {
+  first = "fire";
 }
 
+rand = Math.random();
 if (rand < 0.33) {
-    second = "engine";
-} else if ( rand > 0.33 && rand < 0.66 ) {
-    second = "garments";
-}else {
-    second = "food";
+  second = "engine";
+} else if (rand > 0.33 && rand < 0.66) {
+  second = "garments";
+} else {
+  second = "food";
 }
-
+rand = Math.random();
 if (rand < 0.33) {
-    third = "bros";
-} else if ( rand > 0.33 && rand < 0.66 ) {
-    third = "limited";
-}else {
-    third = "hub";
+  third = "bros";
+} else if (rand > 0.33 && rand < 0.66) {
+  third = "limited";
+} else {
+  third = "hub";
 }
 
 console.log(`${first} ${second} ${third}`);
+
+//using array
+const adjectives = ["crazy", "amazing", "fire"];
+const shopNames = ["Engine", "foods", "garments"];
+const otherWords = ["bros", "limited", "hub"];
+function generateBrand() {
+  let randomAdj = adjectives[Math.floor(Math.random() * adjectives.length)];
+  let randomNames = shopNames[Math.floor(Math.random() * shopNames.length)];
+  let randomWords = otherWords[Math.floor(Math.random() * otherWords.length)];
+
+  console.log(`${randomAdj} ${randomNames} ${randomWords}`);
+}
+
+generateBrand();
